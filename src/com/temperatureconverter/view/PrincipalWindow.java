fce0fb915f138bbd350dd723fce94b4d25f05646
zabieru363 <72569208+zabieru363@users.noreply.github.com>
2022-05-25 18:34:37 +0200

    Botón de iniciar aplicación creado

--- src/com/temperatureconverter/view/PrincipalWindow.java
+++ src/com/temperatureconverter/view/PrincipalWindow.java
@@ -25,27 +25,52 @@ public class PrincipalWindow extends JFrame {
         mainPanel = new javax.swing.JPanel();
         aplicationTitle = new javax.swing.JLabel();
         aplicationLogo = new javax.swing.JLabel();
+        startBtn = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Conversor de Temperaturas");
+        setBounds(new java.awt.Rectangle(0, 0, 500, 800));
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-
-        mainPanel.setLayout(new java.awt.GridLayout(3, 1));
 
         aplicationTitle.setFont(new java.awt.Font("Bookman Old Style", 0, 36)); // NOI18N
         aplicationTitle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         aplicationTitle.setText("Conversor de temperaturas");
-        mainPanel.add(aplicationTitle);
 
         aplicationLogo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         aplicationLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/termometro.png"))); // NOI18N
-        mainPanel.add(aplicationLogo);
+
+        startBtn.setText("Iniciar");
+
+        javax.swing.GroupLayout mainPanelLayout = new javax.swing.GroupLayout(mainPanel);
+        mainPanel.setLayout(mainPanelLayout);
+        mainPanelLayout.setHorizontalGroup(
+            mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(mainPanelLayout.createSequentialGroup()
+                .addGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(aplicationTitle, javax.swing.GroupLayout.PREFERRED_SIZE, 666, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(aplicationLogo, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 666, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(mainPanelLayout.createSequentialGroup()
+                            .addGap(184, 184, 184)
+                            .addComponent(startBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 301, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap())
+        );
+        mainPanelLayout.setVerticalGroup(
+            mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(mainPanelLayout.createSequentialGroup()
+                .addComponent(aplicationTitle, javax.swing.GroupLayout.PREFERRED_SIZE, 174, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(aplicationLogo)
+                .addGap(57, 57, 57)
+                .addComponent(startBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(mainPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 618, Short.MAX_VALUE)
+            .addComponent(mainPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -65,5 +90,6 @@ public class PrincipalWindow extends JFrame {
     private javax.swing.JLabel aplicationLogo;
     private javax.swing.JLabel aplicationTitle;
     private javax.swing.JPanel mainPanel;
+    private javax.swing.JButton startBtn;
     // End of variables declaration//GEN-END:variables
 }
