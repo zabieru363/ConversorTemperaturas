262ace74d1f29052a56e7c1c3b6e86265d02f6b1
zabieru363 <72569208+zabieru363@users.noreply.github.com>
2022-05-25 18:28:06 +0200

    Imagen de la aplicación añadida a la ventana principal

--- src/com/temperatureconverter/view/PrincipalWindow.java
+++ src/com/temperatureconverter/view/PrincipalWindow.java
@@ -23,7 +23,8 @@ public class PrincipalWindow extends JFrame {
     private void initComponents() {
 
         mainPanel = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        aplicationTitle = new javax.swing.JLabel();
+        aplicationLogo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Conversor de Temperaturas");
@@ -31,10 +32,14 @@ public class PrincipalWindow extends JFrame {
 
         mainPanel.setLayout(new java.awt.GridLayout(3, 1));
 
-        jLabel1.setFont(new java.awt.Font("Bookman Old Style", 0, 36)); // NOI18N
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Conversor de temperaturas");
-        mainPanel.add(jLabel1);
+        aplicationTitle.setFont(new java.awt.Font("Bookman Old Style", 0, 36)); // NOI18N
+        aplicationTitle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        aplicationTitle.setText("Conversor de temperaturas");
+        mainPanel.add(aplicationTitle);
+
+        aplicationLogo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        aplicationLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/termometro.png"))); // NOI18N
+        mainPanel.add(aplicationLogo);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -44,7 +49,7 @@ public class PrincipalWindow extends JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(mainPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 566, Short.MAX_VALUE)
+            .addComponent(mainPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -57,7 +62,8 @@ public class PrincipalWindow extends JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel aplicationLogo;
+    private javax.swing.JLabel aplicationTitle;
     private javax.swing.JPanel mainPanel;
     // End of variables declaration//GEN-END:variables
 }
