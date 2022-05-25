eee61c6bce0d605c28a978d620b173bb28ae7608
zabieru363 <72569208+zabieru363@users.noreply.github.com>
2022-05-25 18:56:51 +0200

    Medidas de la ventana de la aplicación establecidas.

--- src/com/temperatureconverter/view/Application.java
+++ src/com/temperatureconverter/view/Application.java
@@ -25,11 +25,11 @@ public class Application extends JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 555, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 420, Short.MAX_VALUE)
         );
 
         pack();
