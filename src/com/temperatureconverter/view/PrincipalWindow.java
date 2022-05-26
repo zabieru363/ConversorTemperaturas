6e40a108948f277db4995d5301688728062506b2
zabieru363 <72569208+zabieru363@users.noreply.github.com>
2022-05-26 17:50:35 +0200

    Redes sociales agregadas en la parte de abajo de la ventana inicial

--- src/com/temperatureconverter/view/PrincipalWindow.java
+++ src/com/temperatureconverter/view/PrincipalWindow.java
@@ -18,7 +18,7 @@ public final class PrincipalWindow extends JFrame {
         setResizable(false);
         setLocationRelativeTo(null);
         initComponents();
-        setBounds(10, 10, 730, 730);
+        setBounds(10, 10, 730, 800);
         // Utilizo la clase Calendar para hacer el año dinámico.
         Calendar cal = Calendar.getInstance();
         int year = cal.get(Calendar.YEAR);
@@ -34,6 +34,10 @@ public final class PrincipalWindow extends JFrame {
         aplicationLogo = new javax.swing.JLabel();
         copyright = new javax.swing.JLabel();
         startBtn = new javax.swing.JButton();
+        socialNetworksPanel = new javax.swing.JPanel();
+        mail = new javax.swing.JLabel();
+        instagram = new javax.swing.JLabel();
+        twitter = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
         setTitle("Conversor de Temperaturas");
@@ -70,6 +74,31 @@ public final class PrincipalWindow extends JFrame {
             }
         });
 
+        socialNetworksPanel.setBackground(new java.awt.Color(51, 51, 51));
+        socialNetworksPanel.setForeground(new java.awt.Color(255, 255, 255));
+        socialNetworksPanel.setLayout(new java.awt.GridLayout(3, 0));
+
+        mail.setFont(new java.awt.Font("Verdana", 0, 12)); // NOI18N
+        mail.setForeground(new java.awt.Color(255, 255, 255));
+        mail.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        mail.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/mail-logo.png"))); // NOI18N
+        mail.setText("zabierujlc@gmail.com");
+        socialNetworksPanel.add(mail);
+
+        instagram.setFont(new java.awt.Font("Verdana", 0, 12)); // NOI18N
+        instagram.setForeground(new java.awt.Color(255, 255, 255));
+        instagram.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        instagram.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/insagram-logo.png"))); // NOI18N
+        instagram.setText("@zabieru.exe");
+        socialNetworksPanel.add(instagram);
+
+        twitter.setFont(new java.awt.Font("Verdana", 0, 12)); // NOI18N
+        twitter.setForeground(new java.awt.Color(255, 255, 255));
+        twitter.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        twitter.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/twitter-logo.png"))); // NOI18N
+        twitter.setText("@Zabieru222");
+        socialNetworksPanel.add(twitter);
+
         javax.swing.GroupLayout mainPanelLayout = new javax.swing.GroupLayout(mainPanel);
         mainPanel.setLayout(mainPanelLayout);
         mainPanelLayout.setHorizontalGroup(
@@ -83,7 +112,9 @@ public final class PrincipalWindow extends JFrame {
                         .addComponent(copyright))
                     .addGroup(mainPanelLayout.createSequentialGroup()
                         .addGap(226, 226, 226)
-                        .addComponent(startBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(socialNetworksPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 220, Short.MAX_VALUE)
+                            .addComponent(startBtn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         mainPanelLayout.setVerticalGroup(
@@ -96,6 +127,8 @@ public final class PrincipalWindow extends JFrame {
                 .addComponent(copyright)
                 .addGap(29, 29, 29)
                 .addComponent(startBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(socialNetworksPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
 
@@ -149,7 +182,11 @@ public final class PrincipalWindow extends JFrame {
     private javax.swing.JLabel aplicationLogo;
     private javax.swing.JLabel aplicationTitle;
     private javax.swing.JLabel copyright;
+    private javax.swing.JLabel instagram;
+    private javax.swing.JLabel mail;
     private javax.swing.JPanel mainPanel;
+    private javax.swing.JPanel socialNetworksPanel;
     private javax.swing.JButton startBtn;
+    private javax.swing.JLabel twitter;
     // End of variables declaration//GEN-END:variables
 }
